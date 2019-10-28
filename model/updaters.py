664d492439431e0684fae3531a39e6cd55a5a1d6
lygztq <lygztq@gmail.com>
2019-10-28 14:27:31 +0800

    NVP updater

--- model/updaters.py
+++ model/updaters.py
@@ -3,14 +3,16 @@ from chainer import training
 import chainer.functions as F
 from data.utils import molecule_id_converter
 from chainer import cuda
+from chainer.dataset import iterator as iterator_module
+
 
 class AtomEmbedUpdater(training.StandardUpdater):
     def __init__(self, iterator, optimizer,
-                 converter=molecule_id_converter, 
-                 device=None, loss_func=None, 
+                 converter=molecule_id_converter,
+                 device=None, loss_func=None,
                  loss_scale=None, auto_new_epoch=True):
-        super().__init__(iterator, optimizer, converter=converter, 
-                         device=device, loss_func=loss_func, 
+        super().__init__(iterator, optimizer, converter=converter,
+                         device=device, loss_func=loss_func,
                          loss_scale=loss_scale, auto_new_epoch=auto_new_epoch)
         self.model = optimizer.target
 
@@ -29,6 +31,40 @@ class AtomEmbedUpdater(training.StandardUpdater):
         acc = F.accuracy(hs, xs)
 
         chainer.report({"ce_loss": loss, "accuracy": acc})
+
+        self.model.cleargrads()
+        loss.backward()
+        optimizer.update()
+
+
+class NVPUpdater(training.StandardUpdater):
+    def __init__(self, iterator, optimizer, 
+                 converter=molecule_id_converter, 
+                 device=None, loss_func=None, 
+                 loss_scale=None, auto_new_epoch=True,
+                 two_step=True, adj_nll_weight=1):
+        super().__init__(
+            iterator, optimizer, converter=converter, 
+            device=device, loss_func=loss_func, 
+            loss_scale=loss_scale, auto_new_epoch=auto_new_epoch)
+        
+        self.model = optimizer.target
+        self.two_step = two_step
+        self.adj_nll_weight = adj_nll_weight
+
+    def update_core(self):
+        batch = self._iterators["main"].next()
+        x, adj = self.converter(batch, self.device)
+        z, sum_log_det_jacs = self.model(x, adj)
+        optimizer = self._optimizers["main"]
+        nll = self.model.log_prob(z, sum_log_det_jacs) # negative log likelihood
+
+        if self.two_step:
+            loss = nll[0] + self.adj_nll_weight * nll[1]
+            chainer.report({"neg_log_likelihood": loss, "nll_x": nll[0], "nll_adj": nll[1]})
+        else:
+            loss = nll
+            chainer.report({"neg_log_likelihood": loss})
 
         self.model.cleargrads()
         loss.backward()
