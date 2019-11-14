8cb7dd562e91ab1f204f7f150a9ffb6310b27136
lygztq <lygztq@gmail.com>
2019-11-14 11:07:14 +0800

    micro change

--- experiment_script/check_model.py
+++ experiment_script/check_model.py
@@ -9,7 +9,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from model.atom_embed import atom_embed
 from model.hyperparameter import Hyperparameter
 from model.nvp_model.nvp_model import AttentionNvpModel
-from data.utils import generate_mols, check_validity, get_atomic_num_id
+from data.utils import generate_mols, check_validity, get_atomic_num_id, \
+    check_novelty, get_validation_idxs, adj_to_smiles
+from chainer_chemistry.datasets import NumpyTupleDataset
 
 if __name__ == "__main__":
     log.basicConfig(level=log.WARN)
@@ -20,30 +22,47 @@ if __name__ == "__main__":
     global_params = Hyperparameter(args.params)
     model_params = global_params.subparams("model")
     config_params = global_params.subparams("configuration")
+    dataset_params = global_params.subparams("dataset")
+
+    # -- load dataset -- #
+    validation_idxs = get_validation_idxs(os.path.join(config_params.root_dir, config_params.train_validation_split))
+    dataset = NumpyTupleDataset.load(os.path.join(
+        dataset_params.root_dir, dataset_params.name))
+    train_idxs = [i for i in range(len(dataset)) if i not in validation_idxs]
+    n_train = len(train_idxs)
+    train_idxs.extend(validation_idxs)
+    train_set = chainer.datasets.SubDataset(
+        dataset, 0, n_train, order=train_idxs)
+    atomic_num_ids = get_atomic_num_id(os.path.join(config_params.root_dir, config_params.atom_id_to_atomic_num))
+    train_smiles = adj_to_smiles(train_set, atomic_num_ids)
 
     model = AttentionNvpModel(model_params)
     model.load_from(args.model)
-    atomic_num_ids = get_atomic_num_id(os.path.join(config_params.root_dir, config_params.atom_id_to_atomic_num))
     device = 0
     model.to_gpu(device)
     chainer.backends.cuda.get_device_from_id(device).use()
     temperatures = np.linspace(0, 1, num=11, dtype=np.float32)
     valids = []
     uniques = []
+    novels = []
     times = 5
 
     with chainer.no_backprop_mode(), chainer.using_config("train", False):
         for t in temperatures:
             temp_valid = []
             temp_unique = []
+            temp_novel = []
             for _ in range(times):
-                xs, adjs = generate_mols(model, temp=t, batch_size=100, device=device)
+                xs, adjs = generate_mols(model, temp=t, batch_size=1000, device=device)
                 res = check_validity(xs, adjs, atomic_num_ids, device=device)
                 temp_valid.append(res["valid_ratio"])
                 temp_unique.append(res["unique_ratio"])
+                temp_novel.append(check_novelty(
+                    res["valid_smiles"], train_smiles))
             valids.append(sum(temp_valid) / times)
             uniques.append(sum(temp_unique) / times)
-            print("temp: {}\tvalid: {}\tunique: {}".format(t, valids[-1], uniques[-1]))
+            novels.append(sum(temp_novel) / times)
+            print("temp: {}\tvalid: {}\tunique: {}\tnovel: {}".format(t, valids[-1], uniques[-1], novels[-1]))
     
     plt.figure()
     plt.grid(True)
@@ -51,6 +70,7 @@ if __name__ == "__main__":
     plt.ylabel("%")
     plt.plot(temperatures, valids, marker="x", label="valid")
     plt.plot(temperatures, uniques, marker="x", label="unique")
+    plt.plot(temperatures, novels, marker="x", label="novel")
     plt.legend()
     plt.show()
     plt.close()
